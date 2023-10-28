635cef1577acede4d68f3da85d57a672502069c1
Paula Perdomo <paulaperdomo@Paulas-MBP.home>
2023-10-27 22:21:52 -0400

    Setting up the project and tests for the app

---
+++ project/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# Create and initialize a new Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
